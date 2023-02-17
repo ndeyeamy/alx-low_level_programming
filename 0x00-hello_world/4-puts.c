87ea61ddd45f568ccab4fca8bafe224a58cee571
ndeyeamy <falamy2006@gmail.com>
2023-02-17 07:34:51 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
 #include<stdio.h>
-/**
- * main - Entry point 
- * Description: 'the program's description'
- * Return: Always 0 (Success)
- */
+/*Return: Always 0 (Success)*/
 int main(void)
 {/*fonction main*/
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
