97fc1e62f148c626c02f8317d2cc18f95b4676dc
ndeyeamy <falamy2006@gmail.com>
2023-02-17 08:03:11 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
   * Ceci est un commentaire en C
   * écrit sur plusieurs ligne
   */
-int main(void)/*fonction main*/
-{/*fonction main*/
+int main(void)
+{
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr);
 return (0);
