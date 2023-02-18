654c6a2d05a6572c52c63266a91de155ccfa9443
ndeyeamy <falamy2006@gmail.com>
2023-02-17 17:18:22 -0800

    is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
+ */
 int main(void)
 {
 	/* betty style doc for function main goes there */
