bbd7112164443214242d913dde42dca61967c5ee
ndeyeamy <falamy2006@gmail.com>
2023-02-19 12:16:40 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: The two digits must be different
+ * eturn: Always 0 (success)
+ */
+int main(void)
+{
+	int j, k;
+
+	for( j = 0 ; j <= 8; ++j)
+	{
+
+		for( k = j + 1; k <= 9; ++k)
+		{
+			printf("%d%d ", j, k);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
