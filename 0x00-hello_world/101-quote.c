20b00291370c0d2ab881f73e4a6a42ff7bfe5dcc
ndeyeamy <falamy2006@gmail.com>
2023-02-20 08:22:10 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-       	const size_t len = sizeof(msg) - 1; 
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+       	const size_t len = sizeof(msg)-1; 
 	write(STDERR_FILENO, msg, len);
 	return (1);
 }
