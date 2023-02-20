f81dbb4bf03ce2088f55608159e7b1e8c58b5dcf
ndeyeamy <falamy2006@gmail.com>
2023-02-20 08:05:34 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: 'prints the size of various types'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+       	const size_t len = sizeof(msg) - 1; 
+	write(STDERR_FILENO, msg, len);
+	return (1);
+}
