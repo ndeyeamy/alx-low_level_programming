fe8302059f262cc5b74ad409c3941317559477e2
ndeyeamy <falamy2006@gmail.com>
2023-02-20 06:58:04 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints the size of various types'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char : %lu octets\n", sizeof(char));
+	printf("Size of an int : %lu octets\n", sizeof(int));
+	printf("Size of a long int: %lu octets\n", sizeof(long));
+	printf("Size of a long long int : %lu octets\n", sizeof(long long));
+	printf("Size of float : %lu octets\n", sizeof(float));
+	return(0);
+}
