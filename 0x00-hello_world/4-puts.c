a19c284fc4496109cd7eff1cdc9a47fd60df270f
ndeyeamy <falamy2006@gmail.com>
2023-02-20 05:30:29 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /*
  * main - Entry point
- * ceci est un commentaire en C
- * écrit sur plusieurs ligne
+ * Use the function puts
+ * not allowed to use printf
+ * program should end with the value 0
  */
+
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
