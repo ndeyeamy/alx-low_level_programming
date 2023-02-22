a4609aa3f63bf8cc4d6a3f2bf639fccc95aeebbf
ndeyeamy <falamy2006@gmail.com>
2023-02-22 12:48:39 -0800

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - affiche 10 fois l'alphabet 
+ * 
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+	for (i = 0 ;i <= 9 ;i++)
+	{
+		for (ch = 'a';ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}	
+}
