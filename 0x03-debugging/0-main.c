198e2fa696bd85bbe5e0b5e79099f69eb668f6fc
ndeyeamy <falamy2006@gmail.com>
2023-02-25 12:47:43 -0800

    positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,54 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
+void print_alphabet(void);
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void);
+/**
+ * _islower - prints if c is lower case
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _islower(int c);
+/**
+ * _isalpha - prints if character is alphabet
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _isalpha(int c);
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0  or -1
+ */
+int print_sign(int n);
+/**
+ * _abs - gives absolute value
+ */
+int _abs(int);
+/**
+ * _abs - gives absolute value
+ */
+int print_last_digit(int n);
+/**
+ * print_last_digit - gives modulo
+ */
+void jack_bauer(void);
+/**
+ * jack_bauer -  print every second 
+ */
+int positive_or_negative(int);
+#endif
