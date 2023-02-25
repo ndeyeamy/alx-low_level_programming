fccf05883e6265f19ccc891aa54a1e38495c1a8e
ndeyeamy <falamy2006@gmail.com>
2023-02-25 15:41:13 -0800

    day of year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -53,4 +53,8 @@ void jack_bauer(void);
 int positive_or_negative(int);
 
 int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
 #endif
