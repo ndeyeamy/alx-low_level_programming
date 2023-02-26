de33997d7d96c77b160ba7849969bcb9c74c3f07
ndeyeamy <falamy2006@gmail.com>
2023-02-26 13:15:35 -0800

    days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || year % 400 == 0 || year % 100 == 0)
 	{
-		if (day >= 1 && day <= 31)
+		if (day != 2 && day <= 31)
 		{
 			day++;
 		}
@@ -23,7 +23,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month >= 12 && day >= 31)
+		if (month == 2 && day >= 30)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
