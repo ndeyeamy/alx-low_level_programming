d2dcbc2b81ada5e976ce573a041ba4b198d8a8ef
ndeyeamy <falamy2006@gmail.com>
2023-02-27 15:56:10 -0800

    ¨swap¨

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 /**
  * reset_to_98 - Entry point
  * Description: 'the program take apointer int and give 98 in output'
- * @n The number to be checke
+ * @n: The number to be checke
  * Return: Always 0 (Success)
  */
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-
+/**
+ * swap_int - Entry point
+ * Description: 'the program take apointer int and give 98 in output'
+ * @a: The number to be checkeà
+ * @b: the number
+ * Return: Always 0 (Success)
+ */
 void swap_int(int *a, int *b)
 {
 	int temp = *a;
