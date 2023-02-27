4c38c4ead30a7571006ead66220303b5bb3b7894
ndeyeamy <falamy2006@gmail.com>
2023-02-27 14:31:58 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * reset_to_98 - eset the int 
  *
  * Return: Always 0.
  */
-void reset_to_98(int *n) {
-    *n = 98;
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
