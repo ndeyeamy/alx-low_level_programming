d8906f0e46d4aba6ea63e487350d5388d2004cc7
ndeyeamy <falamy2006@gmail.com>
2023-03-02 08:15:58 -0800

    loop

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,22 +9,9 @@
 int main(void)
 {
 	/* betty style doc for function main goes there */
-	int n=0;
+	int n = 0;
 
-	srand(time(0));
-	/* n = 0;
-	* your code goes there */
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else
-	{
-		printf("%d is zero\n", n);
-	}
+	printf("%d is zero\n", n); 
+	
 	return (0);
 }
