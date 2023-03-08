46624f5882a2511dfb7184cae3a458ddba29746a
ndeyeamy <falamy2006@gmail.com>
2023-03-08 14:11:12 -0800

    put_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * _puts_recursion - check the code
+ * @s: the parameters
+ * Return: s or 0.
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+#endif
