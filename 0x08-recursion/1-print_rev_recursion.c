ccb98f07225376c868259311f0a4d7342c73507f
ndeyeamy <falamy2006@gmail.com>
2023-03-09 15:34:49 -0800

    count

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * _print_rev_recursion - prints the the reverse recursion 
+ * _print_rev_recursion - prints the the reverse recursion
  * @s: the int to extract the last digit from
  * Return: value of the last digit
  */
 void _print_rev_recursion(char *s)
 {
-	 if (*s)
-	 {
-		 _print_rev_recursion(s + 1);
-		 putchar(*s);
-	 }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
 }
