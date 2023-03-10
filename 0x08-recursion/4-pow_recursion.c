9797cba50f2cd9815b542ab443fe9cd41981914a
ndeyeamy <falamy2006@gmail.com>
2023-03-09 16:48:19 -0800

    sqrt

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - Returns the factorial of a given number.
+ * @x: The number to make power.
+ * @y: the ower
+ * Return: If n > 0 - the power of x.
+ *         If n < 0 - -1 to indicate an error.
+ */
+int _pow_recursion(int x, int y)
+{
+	int pow;
+	if (y < 0)
+		return (-1);
+	else if (y ==0)
+		return (1);
+	pow = x * _pow_recursion(x, y - 1);
+	return (pow);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - Returns the factorial of a given number.
+ * @n: The number to make power.
+ * Return: If n have natural sqrt return result.
+ *         If n have not a natural sqrt return -1.
+ */
+int _sqrt_recursion(int n)
+{
+	int square;
+	if 
