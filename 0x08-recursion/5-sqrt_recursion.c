b9e7d723d021afb63c4dfb152ab00160d2aa2af3
ndeyeamy <falamy2006@gmail.com>
2023-03-09 17:02:40 -0800

    prime

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,7 +6,33 @@
  * Return: If n have natural sqrt return result.
  *         If n have not a natural sqrt return -1.
  */
+/**
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
+}
 int _sqrt_recursion(int n)
 {
-	int square;
-	if 
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
+}
