a4f5925c8596dff6bdce0f924beb6ec62a5a81b1
ndeyeamy <falamy2006@gmail.com>
2023-03-14 04:58:05 -0700

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,16 +15,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	{
-		return NULL;
-	}
-	arr = malloc(size * sizeof(char));
-
+		return (NULL);
+	
+	arr = malloc(sizeof(char) * size);
+	
 	if (arr == NULL)
-	{
-		return NULL;
-	}
-
+		return (NULL);
+	
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
