b6e17fada500a4b07d02e56e692c07d533dc5294
ndeyeamy <falamy2006@gmail.com>
2023-08-24 08:28:51 -0700

    libdy

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -12,6 +12,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
+int _puts(char *s);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
