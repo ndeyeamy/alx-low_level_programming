5390f284f2e2c6cb3cf720596d1c84a0526b0136
ndeyeamy <falamy2006@gmail.com>
2023-08-24 08:01:47 -0700

    libdy

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - print a char
+ * @str: - variable of entry
+ * Return: Always 0 (Success)
+ */
+	int _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+	return (0);
+}
